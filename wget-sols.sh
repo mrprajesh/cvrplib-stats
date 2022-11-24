67e8126dcf244900dab1586ed572f923aedd77e3
Rajesh Pandian M <mrpraj@gmail.com>
2022-11-24 15:29:08 +0530

    get sol script

---
+++ wget-sols.sh
@@ -0,0 +1,148 @@
+#!/bin/bas
+mkdir -p solutions
+cd solutions
+
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Antwerp1.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Antwerp2.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Brussels1.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Brussels2.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Flanders1.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Flanders2.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Ghent1.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Ghent2.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Leuven1.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/XXL/Leuven2.sol
+
+
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/CMT/CMT4.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/CMT/CMT5.sol
+
+
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/E/E-n101-k14.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/E/E-n101-k8.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/E/E-n76-k8.sol
+
+
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/F/F-n135-k7.sol
+
+
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_10.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_11.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_12.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_13.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_14.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_15.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_16.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_17.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_18.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_19.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_20.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/Golden/Golden_9.sol
+
+
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/P/P-n101-k4.sol
+
+
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/tai/tai385.sol
+
+
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n1001-k43.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n101-k25.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n106-k14.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n110-k13.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n115-k10.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n120-k6.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n125-k30.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n129-k18.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n134-k13.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n139-k10.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n143-k7.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n148-k46.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n153-k22.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n157-k13.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n162-k11.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n167-k10.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n172-k51.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n176-k26.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n181-k23.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n186-k15.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n190-k8.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n195-k51.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n200-k36.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n204-k19.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n209-k16.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n214-k11.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n219-k73.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n223-k34.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n228-k23.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n233-k16.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n237-k14.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n242-k48.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n247-k50.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n251-k28.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n256-k16.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n261-k13.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n266-k58.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n270-k35.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n275-k28.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n280-k17.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n284-k15.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n289-k60.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n294-k50.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n298-k31.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n303-k21.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n308-k13.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n313-k71.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n317-k53.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n322-k28.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n327-k20.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n331-k15.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n336-k84.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n344-k43.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n351-k40.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n359-k29.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n367-k17.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n376-k94.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n384-k52.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n393-k38.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n401-k29.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n411-k19.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n420-k130.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n429-k61.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n439-k37.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n449-k29.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n459-k26.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n469-k138.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n480-k70.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n491-k59.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n502-k39.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n513-k21.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n524-k153.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n536-k96.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n548-k50.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n561-k42.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n573-k30.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n586-k159.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n599-k92.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n613-k62.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n627-k43.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n641-k35.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n655-k131.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n670-k130.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n685-k75.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n701-k44.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n716-k35.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n733-k159.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n749-k98.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n766-k71.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n783-k48.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n801-k40.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n819-k171.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n837-k142.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n856-k95.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n876-k59.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n895-k37.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n916-k207.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n936-k151.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n957-k87.sol
+wget http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/X/X-n979-k58.sol
